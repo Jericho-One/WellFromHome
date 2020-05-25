a159d9153ec6cfe80b37d7f8d004167bcdf966c1
Aaron Dishman <>
2020-05-25 16:21:03 -0500

    refactor for blog post

--- app/src/main/java/com/example/wellfromhome/ViewExtensions.kt
+++ app/src/main/java/com/example/wellfromhome/ViewExtensions.kt
@@ -6,6 +6,8 @@ import android.view.animation.AlphaAnimation
 import com.example.wellfromhome.ViewConstants.default_fade_in_duration
 
 fun View.fadeIn(haltTime: Long = 0L, fadeDuration: Long = default_fade_in_duration) {
+    //we set these values first in the case that the XML value
+    //doesn't coincide with the effect we want
     this.visibility = View.INVISIBLE
     Handler().postDelayed({
         this.startAnimation(AlphaAnimation(0F, 1F).apply {
